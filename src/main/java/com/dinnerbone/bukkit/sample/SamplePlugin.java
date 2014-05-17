d59ea512e544b912cb500f1613a3ee1b7ff518c0
deathcap <deathcap@gmx.co.uk>
2014-05-17 13:09:40 -0700

    Add materials sample

--- src/main/java/com/dinnerbone/bukkit/sample/SamplePlugin.java
+++ src/main/java/com/dinnerbone/bukkit/sample/SamplePlugin.java
@@ -2,6 +2,8 @@
 package com.dinnerbone.bukkit.sample;
 
 import java.util.HashMap;
+
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
@@ -51,6 +53,27 @@ public class SamplePlugin extends JavaPlugin {
         // EXAMPLE: Custom code, here we just output some info so we can check all is well
         PluginDescriptionFile pdfFile = this.getDescription();
         getLogger().info( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
+
+        getLogger().info("dirt = " + Material.DIRT);
+
+        checkMaterial("STONE");
+        checkMaterial("DIRT");
+        checkMaterial("SAND");
+    }
+
+    private void checkMaterial(String name) {
+        Material m = Material.getMaterial(name);
+        // test switch() on Material enum
+        switch (m) {
+            case STONE:
+                getLogger().info("got stone");
+                break;
+            case DIRT:
+                getLogger().info("got dirt");
+                break;
+            default:
+                getLogger().info("got else");
+        }
     }
 
     public boolean isDebugging(final Player player) {
